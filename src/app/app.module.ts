9877eb234d030f5ebcb125522c9f7295d52ee761
Rishi22-Coding <saptarshi.patra.22@gmail.com>
2022-11-30 12:30:52 +0530

    Navbar Added using MaterialUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,27 @@ import { AppComponent } from './app.component';
 import { FetchapiComponent } from './components/fetchapi/fetchapi.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FetchapiComponent
+    FetchapiComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
